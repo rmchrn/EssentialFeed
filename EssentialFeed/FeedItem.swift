0aef586526b1008ed611278e582c2963fd3b5760
Ramcharan <ramcharan.gaddam@gmail.com>
2023-02-04 14:52:15 -0600

    create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ramcharan Reddy Gaddam on 04/02/23.
+//
+
+import Foundation
+
+public struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}
